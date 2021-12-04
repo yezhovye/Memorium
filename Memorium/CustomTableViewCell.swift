44bec71835fef7815888d9aebad12b3f61585c79
Sawwaty <yezhovye@mac.com>
2021-12-05 01:35:31 +0300

    outlets are added

---
+++ Memorium/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  Memorium
+//
+//  Created by Victoria Klimova on 05.12.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var commentsLabel: UILabel!
+}
--- Memorium/ViewController.swift
+++ Memorium/ViewController.swift
@@ -23,11 +23,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = placeNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: placeNames[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height/2
-        cell.imageView?.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        cell.nameLabel?.text = placeNames[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: placeNames[indexPath.row])
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height/2
+        cell.imageOfPlace.clipsToBounds = true
         return cell
     }
     
