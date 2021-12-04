0840b03736364b4a44291038f5725b273acb098d
Sawwaty <yezhovye@mac.com>
2021-12-04 18:44:52 +0300

    initial table setup, default places array and photos are added

--- Memorium/ViewController.swift
+++ Memorium/ViewController.swift
@@ -7,11 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let placeNames = [
+        "Moscow Ostankino", "Moscow Klemlin", "Pereslavl Zalesskiy",
+        "Kostroma", "Kolomna", "Gorokhovets",
+        "Sergiev Posad", "Ples", "Vladimir",
+        "Rostov"
+    ]
+    
+    // MARK: - Table view data source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = placeNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: placeNames[indexPath.row])
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
